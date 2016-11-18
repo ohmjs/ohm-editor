e14e7712174771852eaa536a29a11565eeea312d
Patrick Dubroy <pdubroy@gmail.com>
2016-11-18 15:43:56 +0100

    Move parse-tree component into a .vue file.

---
+++ src/components/parse-tree.vue
@@ -0,0 +1,139 @@
+<template>
+  <div id="parseTree">
+    <button v-if="showZoomButton" id="zoomOutButton" type="button"
+            @click="zoomOut" @mouseover="previewZoom" @mouseout="unpreviewZoom">{{
+        zoomButtonLabel
+    }}</button>
+    <div id="visualizerBody">
+      <expanded-input ref="expandedInput" />
+      <parse-results :trace="currentRootTrace" :highlightNode="zoomHighlight"
+                     :measureInputText="measureInputText"
+                     @showContextMenu="showContextMenu"
+                     @updateExpandedInput="updateExpandedInput"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  /* global window */
+  'use strict';
+
+  var cmUtil = require('../cmUtil');
+  var domUtil = require('../domUtil');
+  var isLeaf = require('../traceUtil').isLeaf;
+  var ohmEditor = require('../ohmEditor');
+
+  var expandedInput = require('./expanded-input.vue');
+  var parseResults = require('./parse-results.vue');
+
+  var ANTICLOCKWISE_OPEN_CIRCLE_ARROW = '\u21BA';
+
+  var inputMark;
+  var grammarMark;
+  var defMark;
+
+  // Helpers
+  // -------
+
+  function couldZoom(currentRootTrace, traceNode) {
+    return currentRootTrace !== traceNode &&
+           traceNode.succeeded &&
+           !isLeaf(traceNode);
+  }
+
+  function clearMarks() {
+    inputMark = cmUtil.clearMark(inputMark);
+    grammarMark = cmUtil.clearMark(grammarMark);
+    defMark = cmUtil.clearMark(defMark);
+    ohmEditor.ui.grammarEditor.getWrapperElement().classList.remove('highlighting');
+    ohmEditor.ui.inputEditor.getWrapperElement().classList.remove('highlighting');
+  }
+
+  // Exports
+  // -------
+
+  module.exports = {
+    components: {
+      'expanded-input': expandedInput,
+      'parse-results': parseResults
+    },
+    props: {
+      rootTrace: Object
+    },
+    data: function() {
+      return {
+        zoomTrace: null,
+        previewedZoomTrace: null
+      };
+    },
+    computed: {
+      zoomButtonLabel: function() {
+        return ANTICLOCKWISE_OPEN_CIRCLE_ARROW;
+      },
+      showZoomButton: function() {
+        return this.zoomTrace || this.previewedZoomTrace;
+      },
+      currentRootTrace: function() {
+        return this.zoomTrace || this.rootTrace;
+      },
+      zoomHighlight: function() {
+        if (this.previewedZoomTrace) {
+          return {node: this.previewedZoomTrace, class: 'zoomBorder'};
+        }
+      }
+    },
+    mounted: function() {
+      window.addEventListener('resize', this.$refs.expandedInput.update);
+
+      ohmEditor.addListener('peek:ruleDefinition', function(ruleName) {
+        if (ohmEditor.grammar.rules.hasOwnProperty(ruleName)) {
+          var defInterval = ohmEditor.grammar.rules[ruleName].source;
+          if (defInterval) {
+            var grammarEditor = ohmEditor.ui.grammarEditor;
+            defMark = cmUtil.markInterval(grammarEditor, defInterval, 'active-definition', true);
+            cmUtil.scrollToInterval(grammarEditor, defInterval);
+          }
+        }
+      });
+      ohmEditor.addListener('unpeek:ruleDefinition', clearMarks);
+    },
+    methods: {
+      zoom: function(traceNode) {
+        this.zoomTrace = traceNode;
+        clearMarks();
+      },
+      zoomOut: function() {
+        this.zoomTrace = this.previewedZoomTrace = null;
+      },
+      previewZoom: function() {
+        this.previewedZoomTrace = this.zoomTrace;
+        this.zoomTrace = null;
+      },
+      unpreviewZoom: function() {
+        this.zoomTrace = this.previewedZoomTrace;
+        this.previewedZoomTrace = null;
+      },
+      showContextMenu: function(data) {
+        var zoomEnabled = couldZoom(this.rootTrace, data.traceNode);
+
+        var menuDiv = domUtil.$('#parseTreeMenu');
+        menuDiv.style.left = data.x + 'px';
+        menuDiv.style.top = data.y + 'px';
+        menuDiv.hidden = false;
+
+        var self = this;
+        domUtil.addMenuItem('parseTreeMenu', 'getInfoItem', 'Get Info', false);
+        domUtil.addMenuItem('parseTreeMenu', 'zoomItem', 'Zoom to Node', zoomEnabled, function() {
+          self.zoom(data.traceNode);
+        });
+        ohmEditor.parseTree.emit('contextMenu', data.el, data.traceNode);
+      },
+      updateExpandedInput: function(/* ...args */) {
+        this.$refs.expandedInput.update.apply(null, arguments);
+      },
+      measureInputText: function(text) {
+        return this.$refs.expandedInput.measureText(text);
+      }
+    }
+  };
+</script>
--- src/parseTree.js
+++ src/parseTree.js
@@ -3,156 +3,37 @@
 
 'use strict';
 
-var cmUtil = require('./cmUtil');
-var domUtil = require('./domUtil');
-var isLeaf = require('./traceUtil').isLeaf;
 var ohmEditor = require('./ohmEditor');
 
-var expandedInput = require('./components/expanded-input.vue');
-var parseResults = require('./components/parse-results.vue');
-
-var $ = domUtil.$;
-
-var ANTICLOCKWISE_OPEN_CIRCLE_ARROW = '\u21BA';
-
-var inputMark;
-var grammarMark;
-var defMark;
-
-function clearMarks() {
-  inputMark = cmUtil.clearMark(inputMark);
-  grammarMark = cmUtil.clearMark(grammarMark);
-  defMark = cmUtil.clearMark(defMark);
-  ohmEditor.ui.grammarEditor.getWrapperElement().classList.remove('highlighting');
-  ohmEditor.ui.inputEditor.getWrapperElement().classList.remove('highlighting');
-}
-
-// Trace element helpers
-// ---------------------
-
-function couldZoom(currentRootTrace, traceNode) {
-  return currentRootTrace !== traceNode &&
-         traceNode.succeeded &&
-         !isLeaf(traceNode);
-}
-
-// parse-results
-// -------------
-
-var parseTreeVue = new Vue({
-  el: '#parseTree',
-  components: {
-    'expanded-input': expandedInput,
-    'parse-results': parseResults
-  },
-  data: {
-    rootTrace: null,
-    zoomTrace: null,
-    previewedZoomTrace: null
-  },
-  computed: {
-    zoomButtonLabel: function() {
-      return ANTICLOCKWISE_OPEN_CIRCLE_ARROW;
-    },
-    showZoomButton: function() {
-      return this.zoomTrace || this.previewedZoomTrace;
-    },
-    currentRootTrace: function() {
-      return this.zoomTrace || this.rootTrace;
-    },
-    zoomHighlight: function() {
-      if (this.previewedZoomTrace) {
-        return {node: this.previewedZoomTrace, class: 'zoomBorder'};
-      }
-    }
-  },
-  template: [
-    '<div id="parseTree">',
-    '  <button v-if="showZoomButton" id="zoomOutButton" type="button"',
-    '          @click="zoomOut" @mouseover="previewZoom" @mouseout="unpreviewZoom">{{',
-    '      zoomButtonLabel',
-    '  }}</button>',
-    '  <div id="visualizerBody">',
-    '    <expanded-input ref="expandedInput" />',
-    '    <parse-results :trace="currentRootTrace" :highlightNode="zoomHighlight"',
-    '                   :measureInputText="measureInputText"',
-    '                   @showContextMenu="showContextMenu"',
-    '                   @updateExpandedInput="updateExpandedInput"/>',
-    '  </div>',
-    '</div>'
-  ].join(''),
-  mounted: function() {
-    window.addEventListener('resize', this.$refs.expandedInput.update);
-  },
-  methods: {
-    zoom: function(traceNode) {
-      this.zoomTrace = traceNode;
-      clearMarks();
-    },
-    zoomOut: function() {
-      this.zoomTrace = this.previewedZoomTrace = null;
-    },
-    previewZoom: function() {
-      this.previewedZoomTrace = this.zoomTrace;
-      this.zoomTrace = null;
-    },
-    unpreviewZoom: function() {
-      this.zoomTrace = this.previewedZoomTrace;
-      this.previewedZoomTrace = null;
-    },
-    showContextMenu: function(data) {
-      var zoomEnabled = couldZoom(this.rootTrace, data.traceNode);
-
-      var menuDiv = $('#parseTreeMenu');
-      menuDiv.style.left = data.x + 'px';
-      menuDiv.style.top = data.y + 'px';
-      menuDiv.hidden = false;
-
-      var self = this;
-      domUtil.addMenuItem('parseTreeMenu', 'getInfoItem', 'Get Info', false);
-      domUtil.addMenuItem('parseTreeMenu', 'zoomItem', 'Zoom to Node', zoomEnabled, function() {
-        self.zoom(data.traceNode);
-      });
-      ohmEditor.parseTree.emit('contextMenu', data.el, data.traceNode);
-    },
-    updateExpandedInput: function(/* ...args */) {
-      this.$refs.expandedInput.update.apply(null, arguments);
-    },
-    measureInputText: function(text) {
-      return this.$refs.expandedInput.measureText(text);
-    }
-  }
-});
-
 var parseTree = ohmEditor.parseTree = new CheckedEmitter();
-parseTree.vue = parseTreeVue;
-
-// When the user makes a change in either editor, show the bottom overlay to indicate
-// that the parse tree is out of date.
-function showBottomOverlay(changedEditor) {
-  $('#bottomSection .overlay').style.width = '100%';
-}
-ohmEditor.addListener('change:inputEditor', showBottomOverlay);
-ohmEditor.addListener('change:grammarEditor', showBottomOverlay);
-
-ohmEditor.addListener('peek:ruleDefinition', function(ruleName) {
-  if (ohmEditor.grammar.rules.hasOwnProperty(ruleName)) {
-    var defInterval = ohmEditor.grammar.rules[ruleName].source;
-    if (defInterval) {
-      var grammarEditor = ohmEditor.ui.grammarEditor;
-      defMark = cmUtil.markInterval(grammarEditor, defInterval, 'active-definition', true);
-      cmUtil.scrollToInterval(grammarEditor, defInterval);
+parseTree.vue = new Vue({
+  data: {
+    parsing: false,
+    trace: null
+  },
+  el: '#visualizerContainer',
+  components: {
+    'parse-tree': require('./components/parse-tree.vue')
+  },
+  template:
+    '<div id="visualizerContainer"><parse-tree :rootTrace="trace" />' +
+    '<div v-show="parsing" class="overlay"></div>',
+  methods: {
+    onEdit: function(cm) {
+      this.parsing = true;
     }
+  },
+  mounted: function() {
+    ohmEditor.addListener('change:inputEditor', this.onEdit);
+    ohmEditor.addListener('change:grammarEditor', this.onEdit);
+
+    // Refresh the parse tree after attempting to parse the input.
+    var self = this;
+    ohmEditor.addListener('parse:input', function(matchResult, trace) {
+      self.parsing = false;
+      self.trace = Object.freeze(trace);
+    });
   }
-});
-
-ohmEditor.addListener('unpeek:ruleDefinition', clearMarks);
-
-// Refresh the parse tree after attempting to parse the input.
-ohmEditor.addListener('parse:input', function(matchResult, trace) {
-  $('#bottomSection .overlay').style.width = 0;  // Hide the overlay.
-  $('#semantics').hidden = !ohmEditor.options.semantics;
-  parseTree.vue.rootTrace = Object.freeze(trace);
 });
 
 /*
