4d98da5ac632e61c164d92f46619339cd97f4f11
Patrick Dubroy <pdubroy@gmail.com>
2016-12-15 11:31:50 +0100

    Example worker: prevent error when grammar has no rules.

--- src/exampleWorkerManager.js
+++ src/exampleWorkerManager.js
@@ -36,16 +36,10 @@ function resetWorker(grammar) {
   var examples = ohmEditor.examples.getExamples();
   Object.keys(examples).forEach(function(id) {
     var example = examples[id];
-    var match;
-    if (example.startRule in grammar.rules) {
-      match = grammar.match(example.text, example.startRule);
-    } else {
-      match = grammar.match(example.text);
-    }
-
-    if (match.succeeded()) {
-      exampleWorkerManager.addUserExample(example.startRule || grammar.defaultStartRule,
-                                          example.text);
+    var startRule = example.startRule in grammar.rules ?
+        example.startRule : grammar.defaultStartRule;
+    if (startRule && grammar.match(example.text, startRule).succeeded()) {
+      exampleWorkerManager.addUserExample(startRule, example.text);
     }
   });
 }
@@ -107,7 +101,6 @@ function relayEvent(eventName, args) {
 exampleWorkerManager.neededExamples = null;
 exampleWorkerManager.addListener('received:neededExamples', function(neededExamples) {
   exampleWorkerManager.neededExamples = neededExamples;
-
 });
 
 // Exports
