67e471bc61026654388164126ba78e1b5df1e948
Patrick Dubroy <pdubroy@gmail.com>
2021-10-10 16:21:02 +0200

    Remove electron-related stuff

--- cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-'use strict';
-
-const electronPath = require('electron-prebuilt');
-require('child_process').spawn(electronPath, [__dirname], {
-  stdio: 'inherit',
-  cwd: __dirname,
-});
