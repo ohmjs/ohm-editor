00391282e09f4fb49e5c3aafb3ebf8fedb4cded1
Patrick Dubroy <pdubroy@gmail.com>
2022-05-04 16:37:26 +0200

    chore: Update deployment script

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Deploys portions of this repository (e.g., doc/) to ohmlang.github.io.
 # To run this, you need a checkout of https://github.com/ohmlang/ohmlang.github.io.
 
 # Accepts an optional argument, which is the path to the ohmlang.github.io repository root.
@@ -22,8 +21,8 @@ if [ ! -d "$PAGES_DIR" ]; then
   echo "No such directory: $PAGES_DIR" && exit 1
 fi
 
-# Do a build and copy everything to $PAGES_DIR/editor
-"$ROOT/bin/build-visualizer.sh" "$PAGES_DIR/editor"
+# Do a build and copy everything to $PAGES_DIR/static/editor
+"$ROOT/bin/build-visualizer.sh" "$PAGES_DIR/static/editor"
 
 # Double check that $PAGES_DIR is actually a git repo.
 pushd "$PAGES_DIR"
@@ -31,12 +30,12 @@ if ! git rev-parse --quiet --verify master > /dev/null; then
   echo "Not a git repository: $PAGES_DIR" && exit 1
 fi
 
-read -p "Do you want to deploy to ohmlang.github.io (y/n)? " -n 1 -r
+read -p "Do you want to commit to ohmlang.github.io (y/n)? " -n 1 -r
 
 # Engage!
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   git pull --ff-only --no-stat
-  git add editor
+  git add static/editor
   git commit -m "Update from harc/ohm-editor@${OHM_REV}"
   git push origin master
 fi
