34d46f0d99d20cd1c4b57ecddf6ba543340be0c2
Patrick Dubroy <pdubroy@gmail.com>
2023-03-29 14:04:06 +0200

    More reliable splitter tests (#90)

--- test/splitters-e2e.test.js
+++ test/splitters-e2e.test.js
@@ -1,16 +1,20 @@
 import {test, expect} from '@playwright/test';
-import { initNetworkReplay } from './playwrightHelpers.js';
+import { loadEditorWithArithmeticGrammar } from './playwrightHelpers.js';
 
 test('splitters', async ({page}) => {
-  initNetworkReplay(page);
+  await loadEditorWithArithmeticGrammar(page);
 
-  await page.goto('http://localhost:8080/');
-  await expect(page).toHaveTitle(/Ohm Editor/);
+  const expectInitialScreenshot = async () => {
+    // To avoid inconsistencies with the hover state of the examples, always
+    // hover the first example before taking the screenshot.
+    await page.hover('.example');
+    await expect(page).toHaveScreenshot('topSplitter-before-drag.png');  
+  };
 
-  await expect(page).toHaveScreenshot('topSplitter-before-drag.png');
+  await expectInitialScreenshot();
 
   // 1. Drag the top splitter to the left, making the grammar pane smaller.
-  const topSplitter = page.locator('#topSplitter div');
+  const topSplitter = page.locator('#topSplitter');
   await topSplitter.dragTo(page.locator('body'), {
     targetPosition: { x: 200, y: 400 }
   });
@@ -24,7 +28,7 @@ test('splitters', async ({page}) => {
 
   // 3. Double-click the splitter to return it to its original position.
   await topSplitter.dblclick();
-  await expect(page).toHaveScreenshot('topSplitter-before-drag.png');
+  await expectInitialScreenshot();
 
   // 4. Drag the main splitter up, making the visualizer pane bigger -- far
   // enough that the examples pane isn't fully visible.
@@ -35,6 +39,9 @@ test('splitters', async ({page}) => {
   await expect(page).toHaveScreenshot('mainSplitter-after-drag-1.png');
 
   // 5. Double-click the splitter to return it to its original position.
+  // Ideally, we could compare to 'topSplitter-before-drag.png', but that
+  // doesn't seem to work on Chromium and Firefox, because one of the
+  // examples is hovered at this point.
   await mainSplitter.dblclick();
-  await expect(page).toHaveScreenshot('topSplitter-before-drag.png');
+  await expectInitialScreenshot();
 });
